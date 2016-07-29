4368af3eb9e3097a90314e6a9ad0695119e21f2a
MWiedmeier <mattwiedmeier@gmail.com>
2016-07-28 22:46:06 -0400

    Cleaned up files and added Resume page

--- js/wiedscript.js
+++ js/wiedscript.js
@@ -1,6 +1,12 @@
 $(function(){    
         $(window).scroll(function(){
-            var scrollRatio = $(window).scrollTop() / $(window).height();
-            $('ul').css('background-color', 'rgba(0,0,0,'+(scrollRatio)+')');
+//            var scrollRatio = $(window).scrollTop() / $(window).height();            
+            var ratio = $(window).scrollTop()/ $("#intro-div").offset().top;
+            $('.topnav ul').css('background-color', 'rgba(0,0,0,'+(ratio)+')');
         });
   });
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    document.getElementsByClassName("topnav")[0].classList.toggle("responsive");
+}
