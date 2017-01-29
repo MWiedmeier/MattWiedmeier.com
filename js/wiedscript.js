c4cd9cc54639f732d36d2bc3f41aa9204bb6edfe
MWiedmeier <mattwiedmeier@gmail.com>
2017-01-29 02:30:58 -0800

    Updates?

--- js/wiedscript.js
+++ js/wiedscript.js
@@ -1,10 +1,9 @@
-$(function(){    
-        $(window).scroll(function(){
-//            var scrollRatio = $(window).scrollTop() / $(window).height();            
-            var ratio = $(window).scrollTop()/ $("#intro-div").offset().top;
-            $('.topnav ul').css('background-color', 'rgba(0,0,0,'+(ratio)+')');
-        });
-  });
+//$(function(){    
+//        $(window).scroll(function(){
+//            var ratio = $(window).scrollTop()/ $("#intro-div").offset().top;
+//            $('.topnav ul').css('background-color', 'rgba(0,0,0,'+(ratio)+')');
+//        });
+//  });
 
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
